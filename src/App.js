ea712a975d9b9768297057e7cf76000b382845d4
Armando Ruiz <armando.ruiz@industrie.co>
2019-08-09 14:54:46 +1000

    creando los states y el formulario

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React, {useState, useEffect, Fragment} from 'react';
+import Formulario from './components/Formulario';
 
 function App() {
+  // Se se puede utilizar useState de 2 formas distintas, usar cada state por separado o crear un arreglo.
+  // en este caso vamos a utilizar los states por separado: el artista, la letra (q viene de al API) y la informacion del grupo
+  const [artista, agregarArtista] = useState('');
+  const [letra, agregarLetra] = useState([]); // es un arreglo pq lo vemos en la API
+  const [info, agregarInfo] = useState({});
   return (
-    <div className="App">
-      hola
-    </div>
+    <Fragment>
+      <Formulario />
+
+    </Fragment>
   );
 }
 
